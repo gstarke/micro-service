da86a5eed20d81d47f257764f0964044f05bca07
Garret Starke <gstarke@claritycon.com>
2020-02-21 10:27:28 -0600

    Updated the order class.

--- source/models/Order.hpp
+++ source/models/Order.hpp
@@ -1,13 +1,56 @@
 #ifndef ORDER_HPP_
 #define ORDER_HPP_
 
+enum OrderResult {
+    IllegalOrder = 0,
+    IllegalOrderShipSunk,
+    OrderNotExecuted,
+    Miss,
+    ShipHit,
+    ShipSunk,
+    ShipFound,
+    ShipNotFound,
+    MessageSent
+};
+
+enum OrderType {
+    Attack = 0,
+    Recon,
+    BroadcastMessage
+};
+
 class Order {
 public:
+    // type
+    OrderType type;
+
+    // coordinate
+    Coordinate coordinate = Coordinate(-1, -1);
+
+    // data
+    std::string data;
+
+    // result
+    OrderResult result = OrderResult::OrderNotExecuted;
+
+    // from
+    std::string from;
+
+    // String
+    friend std::ostream& operator<<(std::ostream& os, const Order& obj);
+
 
 protected:
 
 private:
 
 };
+
+// String
+std::ostream& operator<<(std::ostream& os, const Order& obj)
+{
+    os << "Order -- Type: " << obj.type << ", Coordinate: " << obj.coordinate << ", Data: " << obj.data << ", Result: " << obj.result;
+    return os;
+}
 
 #endif // !ORDER_HPP_
